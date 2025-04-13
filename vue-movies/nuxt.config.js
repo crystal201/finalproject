5b8c554d795934265752818473707e41f30293b2
crystal201 <dung20012003@gmail.com>
2025-04-13 17:18:15 +0700

    change

--- vue-movies/nuxt.config.js
+++ vue-movies/nuxt.config.js
@@ -23,6 +23,10 @@ export default {
       implementation: require('sass')
     }
   },
+  eslint: {
+    emitError: false,
+    emitWarning: false
+  },
   server: {
     host: '0.0.0.0',
     port: 3000,
