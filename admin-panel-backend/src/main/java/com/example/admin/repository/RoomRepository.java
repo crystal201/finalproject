ab42aaad720f0430ffc2b8341238feef8709165c
crystal201 <119175068+crystal201@users.noreply.github.com>
2025-06-17 18:06:55 +0700

    Delete admin-panel-backend directory

--- admin-panel-backend/src/main/java/com/example/admin/repository/RoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.admin.repository;
-
-import com.example.admin.entity.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomRepository extends JpaRepository<Room, Long> {
-}
