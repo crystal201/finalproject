5c2d7c2b81ae1419cab9af10ca1b162ee3e2d85c
crystal201 <dung20012003@gmail.com>
2025-06-18 00:32:55 +0700

    resource adminback

--- admin-panel-frontend/vue.config.js
+++ admin-panel-frontend/vue.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   devServer: {
     port: 8082,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    proxy: {
+      '/api': {
+        target: 'http://157.66.219.181:8081/admin',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
   }
 }
