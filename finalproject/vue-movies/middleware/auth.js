c256fc141ce89e4dc7860074b42a9b59044bc67b
root <root@vpssieutoc.1749631346>
2025-06-17 14:30:21 +0700

    Final cleanup: Removed nested 'finalproject' directory from repository

--- finalproject/vue-movies/middleware/auth.js
+++
@@ -1,8 +0,0 @@
-export default function ({ store, redirect, route }) {
-  const protectedRoutes = ['/booking', '/history', '/order'];
-  if (protectedRoutes.includes(route.path)) {
-      if (!store.state.auth.token) {
-          return redirect('/login');
-      }
-  }
-}
