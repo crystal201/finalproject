46ebaf98412b36f815a3e9a9abe9e6c1219ec480
crystal201 <dung20012003@gmail.com>
2025-06-18 22:34:13 +0700

    resource adminback

--- admin-panel-frontend/src/main.js
+++ admin-panel-frontend/src/main.js
@@ -5,8 +5,10 @@ import axios from 'axios';
 import VueAxios from 'vue-axios';
 
 Vue.use(VueRouter);
+const baseURL = process.env.VUE_APP_API_URL || 'http://157.66.219.181:8081';
+console.log('Configured baseURL:', baseURL); // Debug
 Vue.use(VueAxios, axios.create({
-  baseURL: process.env.VUE_APP_API_URL || (process.env.NODE_ENV === 'development' ? '/api' : 'http://157.66.219.181:8081')
+  baseURL: baseURL
 }));
 
 import router from './router';
