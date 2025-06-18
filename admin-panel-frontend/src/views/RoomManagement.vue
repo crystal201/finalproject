89a906b8d81f51b1995c7285375876eb447d5855
crystal201 <dung20012003@gmail.com>
2025-06-18 11:52:39 +0700

    resource adminback

--- admin-panel-frontend/src/views/RoomManagement.vue
+++ admin-panel-frontend/src/views/RoomManagement.vue
@@ -20,7 +20,7 @@ export default {
   methods: {
     async fetchRooms() {
       try {
-        const response = await this.$axios.get('/api/rooms')
+        const response = await this.$axios.get('/rooms')
         this.rooms = response.data
       } catch (error) {
         console.error("API Error:", error)
