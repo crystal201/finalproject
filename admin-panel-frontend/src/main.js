d4f313afaa31a0104b43c44f91bf22baf850270c
crystal201 <dung20012003@gmail.com>
2025-06-19 16:00:28 +0700

    resource adminback

--- admin-panel-frontend/src/main.js
+++ admin-panel-frontend/src/main.js
@@ -5,13 +5,16 @@ import axios from 'axios';
 import VueAxios from 'vue-axios';
 
 Vue.use(VueRouter);
-const baseURL = process.env.NODE_ENV === 'development' 
-  ? 'http://localhost:8081' 
-  : '/api';
 
-Vue.use(VueAxios, axios.create({ baseURL }));
-console.log('Configured baseURL:', baseURL); 
+// Cấu hình axios
+const apiClient = axios.create({
+  baseURL: process.env.NODE_ENV === 'development' 
+    ? 'http://localhost:8081/api'
+    : '/api'
+});
 
+Vue.use(VueAxios, apiClient);
+console.log('Configured axios with baseURL:', apiClient.defaults.baseURL);
 
 import router from './router';
 
