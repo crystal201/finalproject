92e7d68c6facf3a765e7951f9a238ee8b9767a74
crystal201 <dung20012003@gmail.com>
2025-06-19 14:44:23 +0700

    resource adminback

--- admin-panel-frontend/src/views/ManageBookings.vue
+++ admin-panel-frontend/src/views/ManageBookings.vue
@@ -26,7 +26,7 @@ export default {
   methods: {
     async fetchBookings() {
       try {
-        const response = await axios.get('/api/bookings');
+        const response = await this.$axios.get('/api/bookings');
         this.bookings = response.data;
       } catch (error) {
         console.error("API Error:", error);
