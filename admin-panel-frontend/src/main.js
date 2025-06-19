347ffcc7a24d2094b16fa605601f1ed095d24774
crystal201 <dung20012003@gmail.com>
2025-06-19 16:07:06 +0700

    resource adminback

--- admin-panel-frontend/src/main.js
+++ admin-panel-frontend/src/main.js
@@ -6,15 +6,14 @@ import VueAxios from 'vue-axios';
 
 Vue.use(VueRouter);
 
-// Cấu hình axios
 const apiClient = axios.create({
   baseURL: process.env.NODE_ENV === 'development' 
-    ? 'http://localhost:8081/api'
+    ? '/api'
     : '/api'
 });
 
 Vue.use(VueAxios, apiClient);
-console.log('Configured axios with baseURL:', apiClient.defaults.baseURL);
+console.log('Axios configured with baseURL:', apiClient.defaults.baseURL);
 
 import router from './router';
 
