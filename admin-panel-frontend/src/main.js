3f0429b206aa6bbae6e088ce4d792dd7f00a9ccc
crystal201 <dung20012003@gmail.com>
2025-06-20 09:32:09 +0700

    resource adminback

--- admin-panel-frontend/src/main.js
+++ admin-panel-frontend/src/main.js
@@ -7,7 +7,7 @@ import router from './router/index'
 const axiosInstance = axios.create({
   baseURL: process.env.NODE_ENV === 'development' 
     ? 'http://localhost:8081/api' 
-    : '/api'
+    : ''
 })
 
 Vue.use(VueAxios, axiosInstance)
